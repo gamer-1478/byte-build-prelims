7b8c5487faa6a8d1ec28cbbef85a4e391a4c801d
Aayush Garg <aayushgarg2006@gmail.com>
2022-08-17 19:15:51 +0530

    Refactor Server.js

--- server.js
+++ server.js
@@ -2,38 +2,37 @@ require('dotenv').config()
 
 //module s
 const express = require('express'),
-bodyParser = require('body-parser'),
-cors = require('cors'),
-ejs =  require('ejs'),
+    bodyParser = require('body-parser'),
+    cors = require('cors'),
+    ejs = require('ejs'),
     path = require('path'),
     session = require('cookie-session'),
     passport = require('passport'),
-    passportInit  = require('./utils/passportConfig.js'),
+    passportInit = require('./utils/passportConfig.js'),
     flash = require('express-flash'),
     mongoose = require('mongoose');
 
 //routes
-const landing =  require('./routes/landing.js'),
+const landing = require('./routes/landing.js'),
     auth = require('./routes/auth.js'),
     admin = require('./routes/admin.js');
 
-const app = express()
+const app = express(),
+    PORT = 5000 || process.env.PORT;
+
 
 //app middleware
 app.set('view engine', 'ejs')
-app.use(express.static('public'))
-app.use(cors())
-app.use(express.json({limit: '50mb'}))       
-app.use(express.urlencoded({ extended: true, limit: '50mb'})) 
+app.use(express.static('public'), express.json({ limit: '1mb' }), express.urlencoded({ extended: true, limit: '1mb' }), flash())
 app.use('/', express.static('public'))
+
 //passport middleware
 app.use(session({
-    secret:process.env.SESSION_SECRET,
-    resave:true,
-    saveUninitialized:true,
-    cookie:{_expires : 86400000}
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    cookie: { _expires: 86400000 }
 }))
-app.use(flash())
 
 passportInit(passport)
 
@@ -55,8 +54,6 @@ app.use('/auth', auth)
 app.use('/admin', admin)
 
 
-//listen
-const PORT = 5000 || process.env.PORT
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`)
 })
